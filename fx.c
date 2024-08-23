be0cb21f05b95782c4e68fe532b9eee57e7a7fd7
myselfbishal <133786828+myselfbishal@users.noreply.github.com>
2024-08-23 22:43:06 +0530

    Add files via upload

---
+++ fx.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    float x=0.1,result;
+    result = pow(x,3)+exp(4*x)-2*tanh(4*x);
+    printf("THE VALUE OF F(x) IS %f ",result);
+
+return 0;
+
+    
+}
